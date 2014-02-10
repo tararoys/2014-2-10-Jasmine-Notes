fc41ea1a87663cdd310238220a9a43b1062a2064
Tara Roys <tlroys@gmail.com>
2014-02-10 11:32:23 -0600

    Learned how to create an object literal
    An object literal closely resembles a hash. A hash is a toolbag. A key is the name of the tool, and the actual tool is the value of the tool.

--- spec/CommentSpec.js
+++ spec/CommentSpec.js
@@ -2,7 +2,22 @@ describe("Tara is", function() {
   
   describe("Learning How To Set up Jasmine", function(){
     it("should written have a passing test", function() {
-      stuff= expect(true).toBe(true);
+      expect(true).toBe(true);
+    });
+  });
+
+  describe("Learning Object Oriented Programming using", function(){
+    describe("using [Ben's Zoo code](https://gist.github.com/bneuman619/8846586) as a template", function(){
+      it("should create an object literal", function(){
+        my_first_object_literal = {
+          key : "value",
+          another_key : "another_value"
+        };
+        expect(my_first_object_literal.key).toBe("value");
+        expect(my_first_object_literal.another_key).toBe("another_value");
+      });
+
+
     });
   });
 
